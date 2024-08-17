ed88ff83fbc7c2887e977b2f5cb472e4528b2f53
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-08-17 21:05:07 +0300

    feat: Add EJS templates for blog views and partials

---
+++ Authentication & Authorization/config.js
@@ -0,0 +1,15 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "sadik123",
+        database: "blogdb"
+    },
+    email: {
+        username: "<email>",
+        password: "<parola>",
+        from: "<from_email>",
+    }
+}
+
+module.exports = config;
