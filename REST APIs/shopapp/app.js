cef4c759014463d0796a2f119bd5b195445bc09e
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-08-18 20:31:39 +0300

    feat: Add REST API routes for home and products
    - Create `home.js` route file
    - Implement GET request for home route
    - Create `products.js` route file
    - Implement GET request for products route
    - Add dependencies for Express, CORS, and Joi in `package.json`
    - Update `app.js` to use the new routes and dependencies

---
+++ REST APIs/shopapp/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const products = require("./routes/products");
+const home = require("./routes/home");
+
+app.use(express.json());
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET"]
+}));
+
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET");
+//     next();
+// });
+
+app.use("/api/products" ,products);
+app.use("/", home);
+
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
