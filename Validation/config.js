da7f831abc8701dfa1d710cbf9255e0cae2036b5
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-08-18 20:13:42 +0300

    feat: Add EJS templates for blog views, partials, and error pages

---
+++ Validation/config.js
@@ -0,0 +1,15 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "123456",
+        database: "blogdb"
+    },
+    email: {
+        username: "<email>",
+        password: "<parola>",
+        from: "<from_email>",
+    }
+}
+
+module.exports = config;
